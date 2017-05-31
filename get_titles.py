4314eed45044c019e20a6608214ac97b108e6e48
Mohitty <mohitt@iitk.ac.in>
2017-05-31 21:42:22 +0530

    Update get_titles.py

--- get_titles.py
+++ get_titles.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get('https://www.pclun.in')
+driver.get('https://www.pclub.in')
 titles = driver.find_elements_by_css_selector("h2")
 
 for title in titles:
